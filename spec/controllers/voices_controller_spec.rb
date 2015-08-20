d6d90d6ade7f8e81665dd0a4c1b23b801378a984
Jack <organicrails@gmail.com>
2015-08-20 15:30:06 -0700

    Add Controller INDEX test

--- spec/controllers/voices_controller_spec.rb
+++ spec/controllers/voices_controller_spec.rb
@@ -2,5 +2,20 @@ require 'rails_helper'
 
 RSpec.describe VoicesController, type: :controller do
 
+	
+	describe "#index" do
+
+		it "creates an array of voices" do
+			voice = FactoryGirl.create(:voice)
+			get :index
+			expect(assigns(:voices)).to eq([voice])
+		end
+
+		it "renders the index template" do
+			get :index
+			expect(response).to render_template :index
+		end
+	end
+
 
 end
